909084a94e921902cfdb94ee6c14fe05536a8977
Lucasps77 <142064852+Lucasps77@users.noreply.github.com>
2024-04-09 15:27:41 -0300

    Create lista2ex12.c

---
+++ lista2ex12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int maior(int a, int b);
+int main()
+{
+ 
+   int a, b;
+   printf("Digite 2 valores inteiros\n");
+   scanf("%d", &a);
+   scanf("%d", &b);
+   printf("o maior valor é %d", maior(a, b));
+    return 0;
+}
+
+
+int maior(int a, int b){
+     
+    int soma = 0;
+     
+     soma = a + b;
+    
+     return soma;
+}
