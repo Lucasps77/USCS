670d4dfd28f2aa2beaaeba34add7eea4068e4a42
Lucasps77 <142064852+Lucasps77@users.noreply.github.com>
2024-04-09 15:17:44 -0300

    Update lista2ex15.c

--- lista2ex15.c
+++ lista2ex15.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int somar (int vetor[]);
+
+int main()
+{
+ 
+   int vetor[3];
+   
+     
+   printf("Digite 3 valores\n");
+   for(int i = 0; i < 3; i++){
+       scanf("%d", &vetor[i]);
+   }
+
+   int soma = somar(vetor);
+   
+   printf("A soma do valores do vetor %d", soma);
+   
+    return 0;
+}
+
+
+int somar(int vetor[]){
+     
+    int soma = 0;
+    
+    for(int i = 0; i < 3; i++){
+       soma += vetor[i];
+       
+    }
+      return soma;  
+} 
