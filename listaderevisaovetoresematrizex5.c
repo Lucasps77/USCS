70c1c1c20b41cde8415cbb8bf30168650362347f
Lucasps77 <142064852+Lucasps77@users.noreply.github.com>
2024-04-19 15:38:31 -0300

    Create listaderevisaovetoresematrizex5.c

---
+++ listaderevisaovetoresematrizex5.c
@@ -0,0 +1,22 @@
+ex 5
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int vetor[6];
+    int vector[6];
+    
+    
+    for(int i = 0; i < 6; i++){
+        scanf("%d", &vetor[i]);
+    }
+    
+    for(int i = 5; i >= 0; i--){
+        printf("%d ", vetor[i]);
+    }
+    
+
+    return 0;
+}
